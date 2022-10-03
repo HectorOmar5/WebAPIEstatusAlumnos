1500a3209c8524cdf5c6a99b49eba6329fdd0c17
Hector Omar Nava Glz <71728454+HectorOmar5@users.noreply.github.com>
2022-10-03 15:49:22 -0500

    Api de estatus alumno

---
+++ WebAPIEstatusAlumnos/Models/Context/ApiContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using WebAPIEstatusAlumnos.Models.Entities;
+
+#nullable disable
+
+namespace WebAPIEstatusAlumnos.Models.Context
+{
+    public partial class ApiContext : DbContext
+    {
+        public ApiContext()
+        {
+        }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<EstatusAlumnos> EstatusAlumnos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<EstatusAlumnos>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Clave)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("clave")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
